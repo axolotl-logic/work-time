ccaf1637e8c5a29a1dc52b26e58de4c38bb6ca13
Autumn Green <me@axolotl-logic.io>
2025-02-13 13:02:21 -0500

    feat: add daisy UI elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,52 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
+import useInterval from 'use-interval'
+
+const WORK_LENGTH = 50;
+const BREAK_LENGTH = 60 - WORK_LENGTH;
+
+function VisualTimer({
+  status,
+  progress,
+}: {
+  status: "break" | "work";
+  progress: number;
+}) {
+  return (
+    <div className="text-5xl radial-progress text-primary" style={{ "--value": progress, "--size": "300px", "--thickness": "35px" } as React.CSSProperties} role="progressbar">
+      {status}
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const [status, setStatus] = useState<"work" | "break">("work");
+  const [mins, setMins] = useState(0);
+  const [secs, setSecs] = useState(0);
+  
+  useInterval(() => {
+    const now = Date.now();
+    const mins_after_hour =  Math.floor(now / 1000 / 60) % 60;
+    if (mins_after_hour > WORK_LENGTH) {
+      setStatus('break');
+      setMins(WORK_LENGTH + BREAK_LENGTH - mins_after_hour);
+    } else {
+      setStatus('work');
+      setMins(WORK_LENGTH - mins_after_hour);
+    }
+
+    const secs_before_min = 59 - (Math.floor(now / 1000 / 60 / 60) % 60);
+    setSecs(secs_before_min);
+  }, 500);
+
+  const progress = mins / (status == "work" ? WORK_LENGTH : BREAK_LENGTH) * 100;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+    <main className="flex font-mono uppercase min-h-screen flex-col items-center justify-center gap-5">
+      <VisualTimer status={status} progress={progress}/>
+      <div className="text-2xl">
+	{mins}:{String(secs).padStart(2, '0')}
       </div>
     </main>
   );
