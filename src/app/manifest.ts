6795a7aa1caee0fda25f05dbb47af89dd6a03556
Autumn Green <me@axolotl-logic.io>
2025-03-01 22:31:24 -0500

    fix: web manifest paths

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,22 +2,22 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Eternal Work Buddies",
-    short_name: "eternal-buddies",
+    name: "Work Buddies by Axolotl-Logic",
+    short_name: "Work Buddies",
     description:
       "Alternate between work and rest with people across the world.",
     start_url: "/",
     display: "standalone",
-    background_color: "#ffffff",
-    theme_color: "#000000",
+    background_color: "#06050d",
+    theme_color: "#06050d",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
