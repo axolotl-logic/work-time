6fce6b542f2adae6a9aa7f577403136c09979d7f
Autumn Green <me@axolotl-logic.io>
2025-03-12 13:46:00 -0400

    feat: use metadata.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,7 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
-
-export const metadata: Metadata = {
-  title: "Eternal Work Buddies",
-  description: "Alternate between work and rest with people across the world.",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
 
 export default function RootLayout({
   children,
